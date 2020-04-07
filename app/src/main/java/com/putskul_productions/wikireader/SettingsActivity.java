912ce9246520e4326994fb8af185cc842a3884e0
volund <volund@gmail.com>
2020-04-08 00:08:42 +0200

    dictionary language selection

--- app/src/main/java/com/putskul_productions/wikireader/SettingsActivity.java
+++ app/src/main/java/com/putskul_productions/wikireader/SettingsActivity.java
@@ -1,31 +1,54 @@
 package com.putskul_productions.wikireader;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Button mDictionaryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-/*
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            openDrawer();
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
+        mDictionaryButton = (Button)findViewById(R.id.dictionaryButton);
+        mDictionaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDictionaryLanguageDialog();
+            }
+        });
+
+        updateDictionaryButtonText();
+    }
+
+    void updateDictionaryButtonText() {
+        mDictionaryButton.setText("Dictionray: " + Settings.shared.dictionaryLanguage(this));
+    }
+
+    void showDictionaryLanguageDialog() {
+        final String[] dictLangs = Settings.shared.languageMap.get(Settings.shared.sourceLanguage(this));
+        final Context context = this;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setItems(dictLangs, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String dictLang = dictLangs[which];
+                Settings.shared.setDictionaryLanguage(context, dictLang);
+                updateDictionaryButtonText();
+            }
+        });
+        builder.show();
+    }
+
 }
